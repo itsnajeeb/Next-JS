c27f0081c460e9f890b2e21fee208e17461220a0
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-09-02 21:47:14 +0530

    client side navigation using useRouter hook in next js

---
+++ Next Js Basic/next-app/app/Registration/page.jsx
@@ -0,0 +1,107 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+
+const RegistrationForm = () => {
+    const redirect = useRouter()
+
+    const [state, setState] = useState(null)
+    const [formData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+    const submitFormHandler = (e) => {
+        e.preventDefault(); // prevent page refresh
+
+        setState({
+            success: true,
+            message: "Form Submitted Successfully"
+        })
+
+        setFormData({
+            fullName: "",
+            email: "",
+            password: ""
+        });
+
+        redirect.push('/Registration/login')
+        console.log("AFter");
+        
+    }
+    return (
+        <div className=" flex items-center justify-center p-4">
+            <div className="w-full max-w-md bg-gray-600 text-white rounded-2xl shadow-lg p-8">
+                <h2 className="text-2xl font-bold  mb-6 text-center">
+                    Register Yourself
+                </h2>
+                <form className="space-y-4" >
+                    <div>
+                        <label className="block  font-medium mb-1" htmlFor="name" >
+                            Name
+                        </label>
+                        <input
+                            value={formData.fullName}
+                            onChange={handleChange}
+                            name="fullName"
+                            type="text"
+                            id="name"
+                            placeholder="Your Name"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400"
+                        />
+                    </div>
+                    <div>
+                        <label className="block  font-medium mb-1" htmlFor="email" >
+                            Email
+                        </label>
+                        <input
+                            value={formData.email}
+                            onChange={handleChange}
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="you@example.com"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400"
+                        />
+                    </div>
+                    <div>
+                        <label className="block  font-medium mb-1" htmlFor="message">
+                            Password
+                        </label>
+                        <input
+                            value={formData.password}
+                            onChange={handleChange}
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your Password"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400"
+                        />
+                    </div>
+                    <button
+                        onClick={submitFormHandler}
+                        type="submit"
+                        className="w-full bg-[#f70672] cursor-pointer hover:bg-[#c7055d] font-semibold py-2 rounded-lg transition-colors duration-300"
+                    >
+                        Register Now
+                    </button>
+                    {state && (
+                        <p className={`py-2 text-center rounded-lg ${state.success ? 'bg-green-700' : 'bg-red-700'}`}>
+                            {state.message}
+                        </p>
+                    )}
+                </form>
+            </div>
+        </div >
+    );
+};
+
+export default RegistrationForm;
